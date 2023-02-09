904cd7f5e5040bf39e67c194251d1ad2e490f66e
Omprakash Chetiyar <92190792+op0102@users.noreply.github.com>
2023-02-09 22:25:07 +0530

    cors updated aloow origin

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser(""));
-app.use(cors());
+app.use(cors({
+    origin:"*",
+}));
 app.use(morgan("dev"));
 
 app.use(router);
