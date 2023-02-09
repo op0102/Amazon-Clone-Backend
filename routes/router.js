35c9f165002d405946af1ffb4048db3b1537dd38
Omprakash Chetiyar <92190792+op0102@users.noreply.github.com>
2023-02-09 22:53:18 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -7,13 +7,6 @@ const Authenticate = require("../middleware/Authenticate");
 
 
 
-router.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-     });
 
 
 router.get("/getproducts", async (req, res) => {
